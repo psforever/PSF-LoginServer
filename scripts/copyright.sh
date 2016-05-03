14e48d47ae7acf14be8a43bb2dba0c23f96bb073
Chord <chord@tuta.io>
2016-05-03 03:36:44 -0400

    Improve copyright script perf and perserve LE

--- scripts/copyright.sh
+++ scripts/copyright.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -u
 
 COPYRIGHT="// Copyright (c) 2016 PSForever.net to present"
@@ -8,15 +8,16 @@ FILES=$(find . -name '*.scala')
 for f in $FILES; do
 
   LINESPEC=$(grep -n "^${COPYRIGHT}$" "$f")
-  LINESPEC_NOWS=$(grep -n "${COPYRIGHT}" "$f")
-  LINESPEC_OTHER_COPY=$(head -n20 "$f" | grep -ni "copyright")
-
   LINE=$(echo "$LINESPEC" | cut -d: -f1)
 
   if [ ! "$LINE" = "1" ]; then
+    LINESPEC_NOWS=$(grep -n "${COPYRIGHT}" "$f")
+
     if [ ! "$LINESPEC_NOWS" = "" ]; then
       echo "$f: Found but malformed"
     else
+      LINESPEC_OTHER_COPY=$(head -n20 "$f" | grep -ni "copyright")
+
       if [ ! "$LINESPEC_OTHER_COPY" = "" ]; then
         echo "$f: Other copyright found. Skipping..."
       else
@@ -25,7 +26,7 @@ for f in $FILES; do
         read -p "Fix? (y/n) " choice
 
         case $choice in
-          [yY]) sed -i '1i '"$COPYRIGHT"'' "$f" ;;
+          [yY]) sed -i -b '1i '"$COPYRIGHT"'' "$f" ;;
           [nN]) ;;
           *) echo "Invalid choice"; exit 1;;
         esac
