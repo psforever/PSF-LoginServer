8b8194cb441b0d81665e84bc86220942e555d1f9
Chord <chord@tuta.io>
2016-06-04 03:15:02 -0400

    Packet: CharacterRequestMessage naming from RE

--- common/src/main/scala/net/psforever/packet/game/CharacterRequestMessage.scala
+++ common/src/main/scala/net/psforever/packet/game/CharacterRequestMessage.scala
@@ -5,11 +5,18 @@ import net.psforever.packet.{GamePacketOpcode, Marshallable, PacketHelpers, Plan
 import scodec.Codec
 import scodec.codecs._
 
+object CharacterRequestAction extends Enumeration(0) {
+  type Type = Value
+  val Select, Delete, Unused, Unknown3 = Value
+
+  implicit val codec = PacketHelpers.createLongEnumerationCodec(this, uint32L)
+}
+
 /**
   * Is sent by the PlanetSide client when selecting a character to play from the character selection
   * menu.
   */
-final case class CharacterRequestMessage(unk : Long, unk2 : Long)
+final case class CharacterRequestMessage(charId : Long, action: CharacterRequestAction.Type)
   extends PlanetSideGamePacket {
   type Packet = CharacterRequestMessage
   def opcode = GamePacketOpcode.CharacterRequestMessage
@@ -18,7 +25,7 @@ final case class CharacterRequestMessage(unk : Long, unk2 : Long)
 
 object CharacterRequestMessage extends Marshallable[CharacterRequestMessage] {
   implicit val codec : Codec[CharacterRequestMessage] = (
-      ("unk1" | uint32L) ::
-        ("unk2" | uint32L)
+      ("charId" | uint32L) ::
+        ("action" | CharacterRequestAction.codec)
     ).as[CharacterRequestMessage]
 }
