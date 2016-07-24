6720bb942c2a4f5516f89437a7fdf217937604d6
tfarley <tfarleygithub@gmail.com>
2016-07-23 23:57:42 -0700

    Add ChangeAmmoMessage test

--- common/src/test/scala/GamePacketTest.scala
+++ common/src/test/scala/GamePacketTest.scala
@@ -414,5 +414,26 @@ class GamePacketTest extends Specification {
         pkt mustEqual string
       }
     }
+
+    "ChangeAmmoMessage" should {
+      val string = hex"47 4E00 00000000"
+
+      "decode" in {
+        PacketCoding.DecodePacket(string).require match {
+          case ChangeAmmoMessage(item_guid, unk1) =>
+            item_guid mustEqual PlanetSideGUID(78)
+            unk1 mustEqual 0
+          case default =>
+            ko
+        }
+      }
+
+      "encode" in {
+        val msg = ChangeAmmoMessage(PlanetSideGUID(78), 0)
+        val pkt = PacketCoding.EncodePacket(msg).require.toByteVector
+
+        pkt mustEqual string
+      }
+    }
   }
 }
