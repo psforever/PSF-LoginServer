c57980c15d258c3ec0a2ca57744dda91355ba92a
tfarley <tfarleygithub@gmail.com>
2016-07-24 19:26:05 -0700

    Add Vector3 class

--- common/src/main/scala/net/psforever/newcodecs/package.scala
+++ common/src/main/scala/net/psforever/newcodecs/package.scala
@@ -1,10 +1,14 @@
 // Copyright (c) 2016 PSForever.net to present
 package net.psforever.newcodecs
 
+import scodec.Attempt
+import scodec.Attempt.{Failure, Successful}
 import scodec.Codec
 
 package object newcodecs {
 
   def q_double(min: Double, max: Double, bits: Int): Codec[Double] = new QuantizedDoubleCodec(min, max, bits)
+
+  def q_float(min : Double, max : Double, bits : Int): Codec[Float] = q_double(min, max, bits).narrow(v => Attempt.successful(v.toFloat), _.toDouble)
 
 }
