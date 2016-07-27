ae15a7f5c9ab8c0655900f998fb9c264796a1363
tfarley <tfarleygithub@gmail.com>
2016-07-27 16:06:55 -0700

    Add BinaryChoiceCodec

---
+++ common/src/main/scala/net/psforever/newcodecs/BinaryChoiceCodec.scala
@@ -0,0 +1,31 @@
+// Copyright (c) 2016 PSForever.net to present
+package net.psforever.newcodecs
+
+import scodec.{ Codec, SizeBound }
+import scodec.bits.BitVector
+
+private[newcodecs] final class BinaryChoiceCodec[A](choice: Boolean, codec_true: => Codec[A], codec_false: => Codec[A]) extends Codec[A] {
+
+  private lazy val evaluatedCodec_true = codec_true
+  
+  private lazy val evaluatedCodec_false = codec_false
+
+  override def sizeBound = if (choice) evaluatedCodec_true.sizeBound else evaluatedCodec_false.sizeBound
+
+  override def encode(a: A) = {
+    if (choice)
+      evaluatedCodec_true.encode(a)
+    else
+      evaluatedCodec_false.encode(a)
+  }
+
+  override def decode(buffer: BitVector) = {
+    if (choice)
+      evaluatedCodec_true.decode(buffer)
+    else
+      evaluatedCodec_false.decode(buffer)
+  }
+
+  override def toString = if(choice) s"binarychoice(true, $evaluatedCodec_true, ?)" else "binarychoice(false, ?, $evaluatedCodec_false)"
+
+}
