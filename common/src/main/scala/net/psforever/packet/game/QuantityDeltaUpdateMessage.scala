83264de081fd7ad6a50fa23c67344137d33b40e1
Chord <chord@tuta.io>
2016-08-14 16:40:50 -0400

    Packet: QuantityDeltaUpdateMessage (#57)

---
+++ common/src/main/scala/net/psforever/packet/game/QuantityDeltaUpdateMessage.scala
@@ -0,0 +1,29 @@
+// Copyright (c) 2016 PSForever.net to present
+package net.psforever.packet.game
+
+import net.psforever.packet.{GamePacketOpcode, Marshallable, PacketHelpers, PlanetSideGamePacket}
+import scodec.Codec
+import scodec.codecs._
+
+/**
+  * Instructs client to update the quantity of an item based on a delta when sent server to client.
+  *
+  * See also [[QuantityUpdateMessage]]
+  *
+  * @param item_guid the item to update
+  * @param quantity_delta the change in quantity of the item
+  */
+final case class QuantityDeltaUpdateMessage(item_guid : PlanetSideGUID,
+                                            quantity_delta : Int)
+  extends PlanetSideGamePacket {
+  type Packet = QuantityDeltaUpdateMessage
+  def opcode = GamePacketOpcode.QuantityDeltaUpdateMessage
+  def encode = QuantityDeltaUpdateMessage.encode(this)
+}
+
+object QuantityDeltaUpdateMessage extends Marshallable[QuantityDeltaUpdateMessage] {
+  implicit val codec : Codec[QuantityDeltaUpdateMessage] = (
+      ("item_guid" | PlanetSideGUID.codec) ::
+        ("quantity_delta" | int32L)
+    ).as[QuantityDeltaUpdateMessage]
+}
