a4e1e9bc8751c3a0d0aeec3238e20760837ccb4d
FateJH <Jason_DiDonato@yahoo.com>
2016-10-22 17:37:12 -0400

    updating comments

--- common/src/main/scala/net/psforever/packet/game/ZipLineMessage.scala
+++ common/src/main/scala/net/psforever/packet/game/ZipLineMessage.scala
@@ -10,10 +10,10 @@ import scodec.codecs._
   * @param player_guid the player
   * @param origin_side whether this corresponds with the "entry" or the "exit" of the zipline, as per the direction of the light pulse visuals
   * @param unk1 na; usually 0?
-  * @param unk2 na; a number that is consistent to a zipline point but is not a building GUID
-  * @param unk3 na; changes as the user moves
-  * @param unk4 na; changes as the user moves
-  * @param unk5 na; a number that is consistent to a zipline point
+  * @param unk2 na; a number that is consistent to a zipline point but is not a building GUID?
+  * @param unk3 na; changes as the user moves; seems to be related to x or y coordinate of contact
+  * @param unk4 na; changes as the user moves; seems to be related to y or x coordinate of contact
+  * @param unk5 na; changes as the user moves; seems to be related to z (vertical) coordinate of contact
   */
 final case class ZipLineMessage(player_guid : PlanetSideGUID,
                                 origin_side : Boolean,
