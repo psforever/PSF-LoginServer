f081e40f7036c0c64482acfb2a8d6fec3bf3c062
FateJH <Jason_DiDonato@yahoo.com>
2017-01-10 07:54:49 -0500

    modifications to packet before merging

--- common/src/main/scala/net/psforever/packet/game/ChangeShortcutBankMessage.scala
+++ common/src/main/scala/net/psforever/packet/game/ChangeShortcutBankMessage.scala
@@ -11,14 +11,16 @@ import scodec.codecs._
   * The hotbar contains eight slots for user shortcuts - medkits, implants, and text macros.
   * Next to the first slot are up and down arrow buttons with a number.
   * By progressing through the options available from the arrows, eight sets of eight shortcut slots are revealed.
-  * Which set is visible determines the effect of the activating the respective of eight binding keys (the Function keys) for the hotbar.
-  * Each set is called a "bank."
+  * Which set is visible determines the effect of the respective binding keys (the Function keys) for the hotbar.
+  * Each set is called a "bank," obviously.<br>
+  * <br>
+  * This packet coordinates the bank number both as an upstream and as a downstream packet.
   * @param player_guid the player
   * @param bank the shortcut bank (zero-indexed);
   *             0-7 are the valid banks
   */
 final case class ChangeShortcutBankMessage(player_guid : PlanetSideGUID,
-                                  bank : Int)
+                                           bank : Int)
   extends PlanetSideGamePacket {
   type Packet = ChangeShortcutBankMessage
   def opcode = GamePacketOpcode.ChangeShortcutBankMessage
@@ -28,6 +30,6 @@ final case class ChangeShortcutBankMessage(player_guid : PlanetSideGUID,
 object ChangeShortcutBankMessage extends Marshallable[ChangeShortcutBankMessage] {
   implicit val codec : Codec[ChangeShortcutBankMessage] = (
     ("player_guid" | PlanetSideGUID.codec) ::
-      ("bank" | uintL(4))
+      ("bank" | uint4L)
     ).as[ChangeShortcutBankMessage]
 }
