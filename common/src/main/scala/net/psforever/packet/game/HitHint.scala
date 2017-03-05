a3ef4e9a47d74059ea975849a7f51e09696e0a60
FateJH <Jason_DiDonato@yahoo.com>
2017-03-05 01:51:06 -0500

    rebased and updated HitHint packet and tests

--- common/src/main/scala/net/psforever/packet/game/HitHint.scala
+++ common/src/main/scala/net/psforever/packet/game/HitHint.scala
@@ -6,14 +6,18 @@ import scodec.Codec
 import scodec.codecs._
 
 /**
-  * na
-  * @param unk1 na
-  * @param unk2 na
-  * @param x this parameter should not exist
+  * Dispatched by the server to indicate a target or source of damage affecting the player.<br>
+  * <br>
+  * When a source is provided, and within render distance, the player will be shown a fading, outwards drifting, red tick mark.
+  * The location and movement of the mark will indicate a general direction towards the source.
+  * If the option `Game/Show Damage Flash` is set, the player's screen will flash red briefly when a mark is displayed.<br>
+  * <br>
+  * For while some mark is being displayed, the player will also make a grunt of pain.
+  * @param source_guid the source of implied damage
+  * @param player_guid the player
   */
-final case class HitHint(unk1 : Int,
-                         unk2 : Int,
-                         x : Int)
+final case class HitHint(source_guid : PlanetSideGUID,
+                         player_guid : PlanetSideGUID)
   extends PlanetSideGamePacket {
   type Packet = HitHint
   def opcode = GamePacketOpcode.HitHint
@@ -22,8 +26,7 @@ final case class HitHint(unk1 : Int,
 
 object HitHint extends Marshallable[HitHint] {
   implicit val codec : Codec[HitHint] = (
-    ("unk1" | uintL(10)) ::
-      ("unk2" | uintL(10)) ::
-      ("x" | uintL(12))
+    ("source_guid" | PlanetSideGUID.codec) ::
+      ("player_guid" | PlanetSideGUID.codec)
     ).as[HitHint]
 }
