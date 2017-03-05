7b9d97da8b06973e26b27ef2887f58bb25a2625c
FateJH <Jason_DiDonato@yahoo.com>
2017-03-05 13:54:32 -0500

    creating a Vector3 codec for leveraging float values

--- common/src/main/scala/net/psforever/packet/game/GenericCollisionMsg.scala
+++ common/src/main/scala/net/psforever/packet/game/GenericCollisionMsg.scala
@@ -18,12 +18,8 @@ import scodec.codecs._
   * @param target the other party in the collision
   * @param player_health the player's health
   * @param target_health the target's health
-  * @param player_velocity_x the x-component of the player's velocity
-  * @param player_velocity_y the y-component of the player's velocity
-  * @param player_velocity_z the z-component of the player's velocity
-  * @param target_velocity_x the x-component of the target's velocity
-  * @param target_velocity_y the y-component of the target's velocity
-  * @param target_velocity_z the z-component of the target's velocity
+  * @param player_velocity the player's velocity
+  * @param target_velocity the target's velocity
   * @param player_pos the player's world coordinates
   * @param target_pos the target's world coordinates
   * @param unk2 na
@@ -35,12 +31,8 @@ final case class GenericCollisionMsg(unk1 : Int,
                                      target : PlanetSideGUID,
                                      player_health : Int,
                                      target_health : Int,
-                                     player_velocity_x : Float,
-                                     player_velocity_y : Float,
-                                     player_velocity_z : Float,
-                                     target_velocity_x : Float,
-                                     target_velocity_y : Float,
-                                     target_velocity_z : Float,
+                                     player_velocity : Vector3,
+                                     target_velocity : Vector3,
                                      player_pos : Vector3,
                                      target_pos : Vector3,
                                      unk2 : Long,
@@ -59,12 +51,8 @@ object GenericCollisionMsg extends Marshallable[GenericCollisionMsg] {
       ("t" | PlanetSideGUID.codec) ::
       ("p_health" | uint16L) ::
       ("t_health" | uint16L) ::
-      ("p_vel_x" | floatL) ::
-      ("p_vel_y" | floatL) ::
-      ("p_vel_z" | floatL) ::
-      ("t_vel_x" | floatL) ::
-      ("t_vel_y" | floatL) ::
-      ("t_vel_z" | floatL) ::
+      ("p_vel" | Vector3.codec_float) ::
+      ("t_vel" | Vector3.codec_float) ::
       ("p_pos" | Vector3.codec_pos) ::
       ("t_pos" | Vector3.codec_pos) ::
       ("unk2" | uint32L) ::
