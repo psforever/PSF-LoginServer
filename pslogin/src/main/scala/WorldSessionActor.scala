eb27290617d2722c8b6492d42ba4e921aee19f1c
FateJH <Jason_DiDonato@yahoo.com>
2017-03-05 16:54:18 -0500

    correction to WorldSessionActor case

--- pslogin/src/main/scala/WorldSessionActor.scala
+++ pslogin/src/main/scala/WorldSessionActor.scala
@@ -371,7 +371,7 @@ class WorldSessionActor extends Actor with MDCContextAware {
     case msg @ SquadDefinitionActionMessage(a, b, c, d, e, f, g, h, i) =>
       log.info("SquadDefinitionAction: " + msg)
 
-    case msg @ GenericCollisionMsg(u1, p, t, php, thp, pvx, pvy, pvz, tvx, tvy, tvz, ppos, tpos, u2, u3, u4) =>
+    case msg @ GenericCollisionMsg(u1, p, t, php, thp, pv, tv, ppos, tpos, u2, u3, u4) =>
       log.info("Ouch! " + msg)
 
     case msg @ BugReportMessage(version_major,version_minor,version_date,bug_type,repeatable,location,zone,pos,summary,desc) =>
