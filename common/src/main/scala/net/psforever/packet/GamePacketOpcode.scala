06004b0af02b38b36f56d1e0490dbe75bcc96f88
Fate-JH <Jason_DiDonato@yahoo.com>
2017-03-10 21:39:39 -0500

    Packet: PickupItemMessage and create tests (#117)

--- common/src/main/scala/net/psforever/packet/GamePacketOpcode.scala
+++ common/src/main/scala/net/psforever/packet/GamePacketOpcode.scala
@@ -382,7 +382,7 @@ object GamePacketOpcode extends Enumeration {
     case 0x33 => game.ObjectHeldMessage.decode
     case 0x34 => game.WeaponFireMessage.decode
     case 0x35 => game.AvatarJumpMessage.decode
-    case 0x36 => noDecoder(PickupItemMessage)
+    case 0x36 => game.PickupItemMessage.decode
     case 0x37 => game.DropItemMessage.decode
     // 0x38
     case 0x38 => noDecoder(InventoryStateMessage)
--- pslogin/src/main/scala/WorldSessionActor.scala
+++ pslogin/src/main/scala/WorldSessionActor.scala
@@ -283,6 +283,9 @@ class WorldSessionActor extends Actor with MDCContextAware {
       sendResponse(PacketCoding.CreateGamePacket(0, ObjectDetachMessage(PlanetSideGUID(75), item_guid, app.pos, 0, 0, 0)))
       log.info("DropItem: " + msg)
 
+    case msg @ PickupItemMessage(item_guid, player_guid, unk1, unk2) =>
+      log.info("PickupItem: " + msg)
+
     case msg @ ReloadMessage(item_guid, ammo_clip, unk1) =>
       log.info("Reload: " + msg)
       sendResponse(PacketCoding.CreateGamePacket(0, ReloadMessage(item_guid, 123, unk1)))
