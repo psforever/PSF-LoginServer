0b0507617ad40155f68703de4863e0006b0a6ced
FateJH <Jason_DiDonato@yahoo.com>
2017-03-15 19:43:07 -0400

    field names and comments

--- common/src/main/scala/net/psforever/packet/game/ProjectileStateMessage.scala
+++ common/src/main/scala/net/psforever/packet/game/ProjectileStateMessage.scala
@@ -9,19 +9,19 @@ import scodec.codecs._
 /**
   * Dispatched from the server to render the projectiles of one player's weapon on other players' clients.
   * @param projectile_guid the projectile
-  * @param origin a spawning position for the projectile
-  * @param orient a directional heading for the projectile
+  * @param shot_origin a spawning position for the projectile
+  * @param shot_vector a directional heading for the projectile
   * @param unk1 na;
-  *             rarely not 0
+  *             usually 0
   * @param unk2 na
   * @param unk3 na
   * @param unk4 na;
-  *             rarely not false
+  *             usually false
   * @param unk5 na
   */
 final case class ProjectileStateMessage(projectile_guid : PlanetSideGUID,
-                                        origin : Vector3,
-                                        orient : Vector3,
+                                        shot_origin : Vector3,
+                                        shot_vector : Vector3,
                                         unk1 : Int,
                                         unk2 : Int,
                                         unk3 : Int,
@@ -36,8 +36,8 @@ final case class ProjectileStateMessage(projectile_guid : PlanetSideGUID,
 object ProjectileStateMessage extends Marshallable[ProjectileStateMessage] {
   implicit val codec : Codec[ProjectileStateMessage] = (
     ("projectile_guid" | PlanetSideGUID.codec) ::
-      ("origin" | Vector3.codec_pos) ::
-      ("orient" | Vector3.codec_float) ::
+      ("shot_origin" | Vector3.codec_pos) ::
+      ("shot_vector" | Vector3.codec_float) ::
       ("unk1" | uint8L) ::
       ("unk2" | uint8L) ::
       ("unk3" | uint8L) ::
