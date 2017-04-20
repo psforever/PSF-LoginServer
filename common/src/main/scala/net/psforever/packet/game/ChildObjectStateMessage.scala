941b1f01bb7e4714d94a12318c1c4e89ade1fa58
FateJH <Jason_DiDonato@yahoo.com>
2017-04-20 19:02:54 -0400

    comments in the main packet file and working tests

--- common/src/main/scala/net/psforever/packet/game/ChildObjectStateMessage.scala
+++ common/src/main/scala/net/psforever/packet/game/ChildObjectStateMessage.scala
@@ -6,16 +6,26 @@ import scodec.Codec
 import scodec.codecs._
 
 /**
-  * na
+  * Dispatched from a client when its user is controlling a secondary object whose state must be updated.<br>
+  * <br>
+  * When `ChildObjectStateMessage` is being sent to the server, it replaces `PlayerStateMessage`.
+  * The packet frequently gets hidden in a `MultiPacket`, though it is not functionally essential to do that.<br>
+  * <br>
+  * Note the lack of position data.
+  * The secondary object in question is updated in position through another means or is stationary.
+  * The only concern is the direction the object is facing.
+  * The angles are relative to the object's normal forward-facing and typically begin tracking at 0, 0 (forward-facing).
   * @param object_guid the object being manipulated (controlled)
   * @param pitch the angle with respect to the sky and the ground towards which the object is directed;
   *              an 8-bit unsigned value;
-  *              0 is perfectly level and forward-facing, wrapping around at 255;
-  *              positive rotation is occurs by rotating downwards from forward-facing
+  *              0 is perfectly level and forward-facing and mapped to 255;
+  *              positive rotation is downwards from forward-facing;
+  *              between 7 (down) and 231 (up), for 32 angles
   * @param yaw the angle with respect to the horizon towards which the object is directed;
-  *              an 8-bit unsigned value;
-  *              0 is forward-facing, wrapping around at 255;
-  *              positive rotation is clockwise of forward-facing
+  *            an 8-bit unsigned value;
+  *            0 is forward-facing, wrapping around at 127;
+  *            positive rotation is counter-clockwise of forward-facing;
+  *            full rotation
   */
 final case class ChildObjectStateMessage(object_guid : PlanetSideGUID,
                                          pitch : Int,
