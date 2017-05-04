4bc05579cc74fd08bee2140662130a3a5839d6de
SouNourS <sounours@gmail.com>
2017-05-04 13:40:10 +0200

    Update FireHintMessageTest.scala

--- common/src/test/scala/game/FireHintMessageTest.scala
+++ common/src/test/scala/game/FireHintMessageTest.scala
@@ -13,28 +13,28 @@ class FireHintMessageTest extends Specification {
 
   "decode" in {
     PacketCoding.DecodePacket(string).require match {
-      case FireHintMessage(guid1, pos, guid2, guid3, guid4, unk1,u2) =>
-        guid1 mustEqual PlanetSideGUID(5889)
+      case FireHintMessage(weapon_guid, pos, u1, u2, u3, u4, u5) =>
+        weapon_guid mustEqual PlanetSideGUID(5889)
         pos mustEqual Vector3(3482.2734f,3642.4922f,53.125f)
-        guid2 mustEqual 0
-        guid3 mustEqual 65399
-        guid4 mustEqual 7581
-        unk1 mustEqual 0
-        u2 mustEqual None
+        u1 mustEqual 0
+        u2 mustEqual 65399
+        u3 mustEqual 7581
+        u4 mustEqual 0
+        u5 mustEqual None
       case _ =>
         ko
     }
   }
   "decode string2" in {
     PacketCoding.DecodePacket(string2).require match {
-      case FireHintMessage(guid1, pos, guid2, guid3, guid4, unk1,u2) =>
-        guid1 mustEqual PlanetSideGUID(3592)
+      case FireHintMessage(weapon_guid, pos, u1, u2, u3, u4, u5) =>
+        weapon_guid mustEqual PlanetSideGUID(3592)
         pos mustEqual Vector3(2910.789f,3744.875f,69.0625f)
-        guid2 mustEqual 0
-        guid3 mustEqual 65231
-        guid4 mustEqual 64736
-        unk1 mustEqual 3
-        u2 mustEqual Some(Vector3(21.5f,-6.8125f,2.65625f))
+        u1 mustEqual 0
+        u2 mustEqual 65231
+        u3 mustEqual 64736
+        u4 mustEqual 3
+        u5 mustEqual Some(Vector3(21.5f,-6.8125f,2.65625f))
       case _ =>
         ko
     }
