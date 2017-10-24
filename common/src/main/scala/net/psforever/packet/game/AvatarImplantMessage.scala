9c1a8eb6d86392fa9d6a8f72cfe1160a277545ed
SouNourS <sounours@gmail.com>
2017-10-24 13:32:19 +0200

    more search

--- common/src/main/scala/net/psforever/packet/game/AvatarImplantMessage.scala
+++ common/src/main/scala/net/psforever/packet/game/AvatarImplantMessage.scala
@@ -12,9 +12,26 @@ import scodec.codecs._
   * The implant Second Wind is technically an invalid `ImplantType` for this packet.
   * This owes to the unique activation trigger for that implant - a near-death experience of ~0HP.
   * @param player_guid the player
-  * @param action 2 to init, 3 to activate
-  * @param implantSlot na
-  * @param status 0 to uninit or unactivate, 1 to init or activate
+  * @param action
+  *               0 : add implant
+  *                   with status = 0 to 9 (from ImplantType)
+  *               1 : remove implant
+  *                   seems work with any value in status
+  *               2 : init implant
+  *                   status : 0 to "uninit"
+  *                   status : 1 to init
+  *               3 : activate implant
+  *                   status : 0 to desactivate
+  *                   status : 1 to activate
+  *               4 : number of implant slots unlocked
+  *                   status : 0 = no implant slot
+  *                   status : 1 = first implant slot + "implant message"
+  *                   status : 2 or 3 = unlock second & third slots
+  *               5 : out of stamina message
+  *                   status : 0 to stop the lock
+  *                   status : 1 to active the lock
+  * @param implantSlot : from 0 to 2
+  * @param status : see action
   */
 final case class AvatarImplantMessage(player_guid : PlanetSideGUID,
                                       action : Int,
