c33477eaac608673fb1d321699b441d3b381ca07
Chord <chord@tuta.io>
2019-10-22 23:05:11 -0400

    Config: add developer.Netsim.*
    Break out configuration of the UDPNetworkSimulator to worldserver.ini

--- pslogin/src/main/scala/WorldConfig.scala
+++ pslogin/src/main/scala/WorldConfig.scala
@@ -1,5 +1,6 @@
 // Copyright (c) 2019 PSForever
 import net.psforever.config._
+import scala.concurrent.duration._
 
 object WorldConfig extends ConfigParser {
   protected var config_map : Map[String, Any] = Map()
@@ -10,6 +11,13 @@ object WorldConfig extends ConfigParser {
     ),
     ConfigSection("worldserver",
       ConfigEntryInt("ListeningPort", 51001, Constraints.min(1), Constraints.max(65535))
+    ),
+    ConfigSection("developer",
+      ConfigEntryBool ("NetSim.Active", false),
+      ConfigEntryFloat("NetSim.Loss", 0.02f, Constraints.min(0.0f), Constraints.max(1.0f)),
+      ConfigEntryTime ("NetSim.Delay", 150 milliseconds, Constraints.min(0 seconds), Constraints.max(2 seconds)),
+      ConfigEntryFloat("NetSim.ReorderChance", 0.005f, Constraints.min(0.0f), Constraints.max(1.0f)),
+      ConfigEntryTime ("NetSim.ReorderTime", 150 milliseconds, Constraints.min(0 seconds), Constraints.max(2 seconds))
     )
   )
 
