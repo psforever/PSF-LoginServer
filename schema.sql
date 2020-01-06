3f86534b6577d7f46e506e18c225f441bcd17623
Chord <chord@tuta.io>
2020-01-06 22:11:26 +0100

    Add database setup documentation

--- schema.sql
+++ schema.sql
@@ -1,11 +1,3 @@
-CREATE DATABASE psforever
-  WITH OWNER = postgres
-       ENCODING = 'UTF8'
-       TABLESPACE = pg_default
-       LC_COLLATE = 'English_United States.1252'
-       LC_CTYPE = 'English_United States.1252'
-       CONNECTION LIMIT = -1;
-
 CREATE TABLE IF NOT EXISTS "accounts" (
   "id" SERIAL PRIMARY KEY NOT NULL,
   "username" VARCHAR(64) NOT NULL UNIQUE,
