0c1486dbcc22f85e4fa0d40ec0bedce5eee6f25c
FateJH <Jason_DiDonato@yahoo.com>
2020-01-07 09:20:12 -0500

    revamped IdentifiableEntity to work with a revamped PlanetSideGUID; GUID's now have a characteristic that they can be valid or stale (this is a container-level distinction, now a value-level distinction); all appropriate behaviors should be roughly the same as before the changes

--- common/src/main/scala/net/psforever/objects/entity/IdentifiableEntity.scala
+++ common/src/main/scala/net/psforever/objects/entity/IdentifiableEntity.scala
@@ -1,104 +1,110 @@
-// Copyright (c) 2017 PSForever
+// Copyright (c) 2017-2019 PSForever
 package net.psforever.objects.entity
 
-import net.psforever.types.PlanetSideGUID
+import net.psforever.types.{PlanetSideGUID, StalePlanetSideGUID}
 
 /**
-  * Represent any entity that must have its own globally unique identifier (GUID) to be functional.<br>
+  * Represent any entity that must have its own valid global unique identifier (GUID) to be functional.<br>
   * <br>
-  * "Testing" the object refers to the act of acquiring a reference to the GUID the object is using.
-  * This object starts with a container class that represents a unprepared GUID state and raises an `Exception` when tested.
-  * Setting a proper `PlanetSideGUID` replaces that container class with a container class that returns the GUID when tested.
-  * The object can be invalidated, retaining the previous identifier number, but marking that object as being "stale."
-  * "Staleness" is a property indicating whether or not the number can be used as a valid representation of the object.
+  * The basic design philosophy of the workflow of a GUID at this stage is a deterministic state machine.
+  * At the start, an `Exception` will be thrown while the default conditions of the accessor and mutator are maintained.
+  * ("The ability to set a new valid GUID".)
+  * Only a valid GUID may be set and, once it does, that changes the conditions of the accessor and mutator
+  * to one where it will return the valid GUID and one where it will no longer accept a new GUID (valid or invalid).
+  * That GUID will continue being the GUID reported by the object, even if another valid GUID tries to be set.
+  * (No error or exception will be thrown.)
+  * To set a new GUID, the current one must be invalidated with the appropriate function,
+  * and this turns both the object and any object reference that can be acquired from the object "stale."
+  * Doing this prior to setting the initial valid GUID is fruitless
+  * as it restores the object to its default mutation option ("the ability to set a new valid GUID").
+  * Access to the GUID is retained.
+  * This can be done as many times as is necessary by following the same order of actions.<br>
+  * <br>
+  * The "staleness" of the object and the "staleness" of the GUID are related.
+  * The condition in general indicates that the object has somehow become externally disconnected from its GUID reference
+  * though the two still share something similar to their prior relationship internally.
+  * Do not expect a "stale" GUID to refer to the same object through some mapping mechanism.
+  * Do not expect a "stale" object to give you a GUID that will map back to itself.
   * @throws `NoGUIDException` if a GUID has not yet been assigned
   */
 abstract class IdentifiableEntity extends Identifiable {
+  /** storage for the GUID information; starts as a `StalePlanetSideGUID` */
+  private var current : PlanetSideGUID = StalePlanetSideGUID(0)
   /** indicate the validity of the current GUID */
-  private var stale : Boolean = true
-  /** storage for the active GUID */
-  private val container : GUIDContainable = GUIDContainer()
-  /** the handle for the active GUID; starts as exception-throwing */
-  private var current : GUIDContainable = IdentifiableEntity.noGUIDContainer
+  private var guidValid : Boolean = false
+  /** the current accessor; can be re-assigned */
+  private var guidAccessor : IdentifiableEntity=>PlanetSideGUID = IdentifiableEntity.noGuidGet
+  /** the current mutator; can be re-assigned */
+  private var guidMutator : (IdentifiableEntity, PlanetSideGUID)=>PlanetSideGUID = IdentifiableEntity.noGuidSet
+
+  def GUID : PlanetSideGUID = guidAccessor(this)
+
+  /** Always intercept `StalePlanetSideGUID` references when attempting to mutate the GUID value. */
+  def GUID_=(guid : StalePlanetSideGUID) : PlanetSideGUID = guidAccessor(this)
+
+  def GUID_=(guid : PlanetSideGUID) : PlanetSideGUID = guidMutator(this, guid)
 
   /**
-    * The object will not originally having a valid GUID,
-    * so "stale" will be used to expressed "not initialized."
-    * After being set and then properly invalidated, then it will indicate proper staleness.
+    * Flag when the object has no GUID (initial condition) or is considered stale.
     * @return whether the value of the GUID is a valid representation for this object
     */
-  def HasGUID : Boolean = !stale
-
-  def GUID : PlanetSideGUID = current.GUID
-
-  def GUID_=(guid : PlanetSideGUID) : PlanetSideGUID = {
-    stale = false
-    current = container
-    current.GUID = guid
-    GUID
-  }
+  def HasGUID : Boolean = guidValid
 
   /**
-    * Set the staleness to indicate whether the GUID has ever been set
-    * or that the set GUID is not a proper representation of the object.
-    * It is always set to `true`.
+    * Indicate that the current GUID is no longer a valid representation of the object.
+    * Transforms whatever the current GUID is into a `StalePlanetSideGUID` entity with the same value.
+    * Doing this restores the object to its default mutation option ("the ability to set a new valid GUID").
+    * The current GUID will still be accessed as if it were valid, but it will be wrapped in the new stale object.
     */
   def Invalidate() : Unit = {
-    stale = true
+    guidValid = false
+    current = StalePlanetSideGUID(current.guid)
+    guidMutator = IdentifiableEntity.noGuidSet
   }
 }
 
 object IdentifiableEntity {
-  private val noGUIDContainer : GUIDContainable = new NoGUIDContainer
-}
-
-/**
-  * Mask the `Identifiable` `trait`.
-  */
-sealed trait GUIDContainable extends Identifiable
-
-/**
-  * Hidden container that represents an object that is not ready to be used by the game.
-  */
-private case class NoGUIDContainer() extends GUIDContainable {
   /**
-    * Raise an `Exception` because we have no GUID to give.
+    * Raise an `Exception` because the entity is never considered having a GUID to give.
+    * @param o the any entity with a GUID
     * @throws `NoGUIDException` always
     * @return never returns
     */
-  def GUID : PlanetSideGUID = {
-    throw NoGUIDException(s"object $this has not initialized a global identifier")
+  def noGuidGet(o : IdentifiableEntity) : PlanetSideGUID = {
+    throw NoGUIDException(s"did not initialize this object $o with a valid global identifier")
   }
 
   /**
-    * Normally, this should never be called.
-    * @param toGuid the globally unique identifier
-    * @return never returns
-    */
-  def GUID_=(toGuid : PlanetSideGUID) : PlanetSideGUID = {
-    throw NoGUIDException("can not initialize a global identifier with this object")
-  }
-}
-
-/**
-  * Hidden container that represents an object that has a working GUID and is ready to be used by the game.
-  * @param guid the object's globally unique identifier;
-  *             defaults to a GUID equal to 0
-  */
-private case class GUIDContainer(private var guid : PlanetSideGUID = PlanetSideGUID(0)) extends GUIDContainable {
-  /**
-    * Provide the GUID used to initialize this object.
+    * Provide the entity with a valid GUID replacing an invalid GUID.
+    * Modify the accessor and mutator function literals to ensure the entity will remain stable.
+    * It will not be mutated by a new valid value without the existing valid value having to first be invalidated.
+    * Its access is made standard.
+    * @param o the any entity with a GUID
+    * @param guid the valid GUID to assign
     * @return the GUID
     */
-  def GUID : PlanetSideGUID = guid
+  def noGuidSet(o : IdentifiableEntity, guid : PlanetSideGUID) : PlanetSideGUID = {
+    o.current = guid
+    o.guidValid = true
+    o.guidAccessor = guidGet
+    o.guidMutator = guidSet
+    guid
+  }
 
   /**
-    * Exchange the previous GUID for a new one, re-using this container.
-    * @param toGuid the globally unique identifier
-    * @return the GUID
+    * The entity should have a valid GUID that can be provided.
+    * @param o the entity
+    * @return the entity's GUID
     */
-  def GUID_=(toGuid : PlanetSideGUID) : PlanetSideGUID = {
-    guid = toGuid
-    GUID
-  }
+  def guidGet(o : IdentifiableEntity) : PlanetSideGUID = o.current
+
+  /**
+    * The entity is in a condition where it can not be assigned the new valid GUID.
+    * This state establishes itself after setting the very first valid GUID and
+    * will persist to the end of the entity's life.
+    * @param o the any entity with a GUID
+    * @param guid the valid GUID to assign
+    * @return the entity's GUID
+    */
+  def guidSet(o : IdentifiableEntity, guid : PlanetSideGUID) : PlanetSideGUID = o.current
 }
