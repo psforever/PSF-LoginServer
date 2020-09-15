9f0f3fe712404adef65d322272273a84c35d9d17
Jason_DiDonato@yahoo.com <Jason_DiDonato@yahoo.com>
2020-09-15 19:35:41 -0400

    modifiers for flak burst

--- src/main/scala/net/psforever/objects/vital/damage/DamageModifiers.scala
+++ src/main/scala/net/psforever/objects/vital/damage/DamageModifiers.scala
@@ -47,6 +47,7 @@ object DamageModifiers {
     private def function(damage: Int, data: ResolvedProjectile): Int = damage
   }
 
+  /** If the calculated distance is greater than the maximum distance of the projectile, damage is zero'd. */
   case object MaxDistanceCutoff extends Mod {
     def Calculate: DamageModifiers.Format = function
 
@@ -62,6 +63,21 @@ object DamageModifiers {
     }
   }
 
+  /** If the calculated distance is greater than a custom distance, damage is zero'd. */
+  case class CustomDistanceCutoff(cutoff: Float) extends Mod {
+    def Calculate: DamageModifiers.Format = function
+
+    private def function(damage: Int, data: ResolvedProjectile): Int = {
+      val projectile = data.projectile
+      val distance   = Vector3.Distance(data.hit_pos, projectile.shot_origin)
+      if (distance <= cutoff) {
+        damage
+      } else {
+        0
+      }
+    }
+  }
+
   /**
     * The input value degrades (lessens)
     * the further the distance between the point of origin (`shot_origin`)
@@ -70,22 +86,7 @@ object DamageModifiers {
     * If the value is encountered beyond its maximum distance, the value is zero'd.
     */
   case object DistanceDegrade extends Mod {
-    def Calculate: DamageModifiers.Format = function
-
-    private def function(damage: Int, data: ResolvedProjectile): Int = {
-      val projectile = data.projectile
-      val profile    = projectile.profile
-      val distance   = Vector3.Distance(data.hit_pos, projectile.shot_origin)
-      if (distance <= profile.DistanceMax) {
-        if (profile.DistanceNoDegrade == profile.DistanceMax || distance <= profile.DistanceNoDegrade) {
-          damage
-        } else {
-          damage - ((damage - profile.DegradeMultiplier * damage) * ((distance - profile.DistanceNoDegrade) / (profile.DistanceMax - profile.DistanceNoDegrade))).toInt
-        }
-      } else {
-        0
-      }
-    }
+    def Calculate: DamageModifiers.Format = distanceDegradeFunction
   }
 
   /**
@@ -95,24 +96,7 @@ object DamageModifiers {
     * If the value is encountered beyond its maximum radial distance, the value is zero'd.
     */
   case object RadialDegrade extends Mod {
-    def Calculate: DamageModifiers.Format = function
-
-    private def function(damage: Int, data: ResolvedProjectile): Int = {
-      val profile   = data.projectile.profile
-      val distance  = Vector3.Distance(data.hit_pos, data.target.Position)
-      val radius    = profile.DamageRadius
-      val radiusMin = profile.DamageRadiusMin
-      if (distance <= radiusMin) {
-        damage
-      } else if (distance <= radius) {
-        //damage - (damage * profile.DamageAtEdge * (distance - radiusMin) / (radius - radiusMin)).toInt
-        val base = profile.DamageAtEdge
-        val radi = radius - radiusMin
-        (damage * ((1 - base) * ((radi - (distance - radiusMin)) / radi) + base)).toInt
-      } else {
-        0
-      }
-    }
+    def Calculate: DamageModifiers.Format = radialDegradeFunction
   }
 
   /**
@@ -139,7 +123,7 @@ object DamageModifiers {
   }
 
   /*
-  Below this point are the calculations for sources of aggravated damage.
+  Aggravated damage.
   For the most part, these calculations are individualistic and arbitrary.
   They exist in their current form to satisfy observed shots to kill (STK) of specific weapon systems
   according to 2012 standards of the Youtube video series by TheLegendaryNarwhal.
@@ -150,7 +134,7 @@ object DamageModifiers {
     */
   case object InfantryAggravatedDirect extends Mod {
     def Calculate: DamageModifiers.Format =
-      BaseAggravatedFormula(ProjectileResolution.AggravatedDirect, DamageType.Direct)
+      baseAggravatedFormula(ProjectileResolution.AggravatedDirect, DamageType.Direct)
   }
 
   /**
@@ -159,7 +143,7 @@ object DamageModifiers {
     */
   case object InfantryAggravatedSplash extends Mod {
     def Calculate: DamageModifiers.Format =
-      BaseAggravatedFormula(ProjectileResolution.AggravatedSplash, DamageType.Splash)
+      baseAggravatedFormula(ProjectileResolution.AggravatedSplash, DamageType.Splash)
   }
 
   /**
@@ -169,7 +153,7 @@ object DamageModifiers {
     */
   case object InfantryAggravatedDirectBurn extends Mod {
     def Calculate: DamageModifiers.Format =
-      BaseAggravatedBurnFormula(ProjectileResolution.AggravatedDirectBurn, DamageType.Direct)
+      baseAggravatedBurnFormula(ProjectileResolution.AggravatedDirectBurn, DamageType.Direct)
   }
 
   /**
@@ -179,110 +163,7 @@ object DamageModifiers {
     */
   case object InfantryAggravatedSplashBurn extends Mod {
     def Calculate: DamageModifiers.Format =
-      BaseAggravatedBurnFormula(ProjectileResolution.AggravatedSplashBurn, DamageType.Splash)
-  }
-
-  /**
-    * For damage application that involves aggravation of a particular damage type,
-    * calculate that initial damage application for infantry targets
-    * and produce the modified damage value.
-    * Infantry wearing mechanized assault exo-suits (MAX) incorporate an additional modifier.
-    * @see `AggravatedDamage`
-    * @see `ExoSuitType`
-    * @see `InfantryAggravatedDirect`
-    * @see `InfantryAggravatedSplash`
-    * @see `PlayerSource`
-    * @see `ProjectileTarget.AggravatesTarget`
-    * @see `ResolvedProjectile`
-    * @param resolution the projectile resolution to match against
-    * @param damageType the damage type to find in as a component of aggravated information
-    * @param damage the base damage value
-    * @param data historical information related to the damage interaction
-    * @return the modified damage
-    */
-  private def BaseAggravatedFormula(
-                                     resolution: ProjectileResolution.Value,
-                                     damageType : DamageType.Value
-                                   )
-                                   (
-                                     damage: Int,
-                                     data: ResolvedProjectile
-                                   ): Int = {
-    if (data.resolution == resolution &&
-      data.projectile.quality == ProjectileQuality.AggravatesTarget) {
-      (data.projectile.profile.Aggravated, data.target) match {
-        case (Some(aggravation), p: PlayerSource) =>
-          val aggravatedDamage = aggravation.info.find(_.damage_type == damageType) match {
-            case Some(infos) =>
-              damage * infos.degradation_percentage + damage
-            case _ =>
-              damage toFloat
-          }
-          if(p.ExoSuit == ExoSuitType.MAX) {
-            (aggravatedDamage * aggravation.max_factor) toInt
-          } else {
-            aggravatedDamage toInt
-          }
-        case _ =>
-          damage
-      }
-    } else {
-      damage
-    }
-  }
-
-  /**
-    * For damage application that involves aggravation of a particular damage type,
-    * calculate that damage application burn for each tick for infantry targets
-    * and produce the modified damage value.
-    * Infantry wearing mechanized assault exo-suits (MAX) incorporate an additional modifier.
-    * Vanilla infantry incorporate their resistance value into a slightly different calculation than usual.
-    * @see `AggravatedDamage`
-    * @see `ExoSuitType`
-    * @see `InfantryAggravatedDirectBurn`
-    * @see `InfantryAggravatedSplashBurn`
-    * @see `PlayerSource`
-    * @see `ResolvedProjectile`
-    * @param resolution the projectile resolution to match against
-    * @param damageType the damage type to find in as a component of aggravated information
-    * @param damage the base damage value
-    * @param data historical information related to the damage interaction
-    * @return the modified damage
-    */
-  private def BaseAggravatedBurnFormula(
-                                         resolution: ProjectileResolution.Value,
-                                         damageType : DamageType.Value
-                                       )
-                                       (
-                                         damage: Int,
-                                         data: ResolvedProjectile
-                                       ): Int = {
-    if (data.resolution == resolution) {
-      (data.projectile.profile.Aggravated, data.target) match {
-        case (Some(aggravation), p: PlayerSource) =>
-          val degradation = aggravation.info.find(_.damage_type == damageType) match {
-            case Some(info) =>
-              info.degradation_percentage
-            case _ =>
-              1f
-          }
-          if (p.exosuit == ExoSuitType.MAX) {
-            (damage * degradation * aggravation.max_factor) toInt
-          } else {
-            val resist = data.damage_model.ResistUsing(data)(data)
-            //add resist to offset resist subtraction later
-            if (damage > resist) {
-              ((damage - resist) * degradation).toInt + resist
-            } else {
-              (damage * degradation).toInt + resist
-            }
-          }
-        case _ =>
-          0
-      }
-    } else {
-      damage
-    }
+      baseAggravatedBurnFormula(ProjectileResolution.AggravatedSplashBurn, DamageType.Splash)
   }
 
   /**
@@ -295,8 +176,8 @@ object DamageModifiers {
 
     private def formula(damage: Int, data: ResolvedProjectile): Int = {
       if (damage > 0 &&
-          data.resolution == ProjectileResolution.AggravatedDirectBurn ||
-          data.resolution == ProjectileResolution.AggravatedSplashBurn) {
+          (data.resolution == ProjectileResolution.AggravatedDirectBurn ||
+           data.resolution == ProjectileResolution.AggravatedSplashBurn)) {
         //add resist to offset resist subtraction later
         1 + data.damage_model.ResistUsing(data)(data)
       } else {
@@ -425,6 +306,17 @@ object DamageModifiers {
     }
   }
 
+  /**
+    * If the projectile has charging properties,
+    * and the weapon that produced the projectile has charging mechanics,
+    * calculate the current value of the damage as a sum
+    * of some minimum damage and scaled normal damage.
+    * The projectile quality has information about the "factor" of damage scaling.
+    * @see `ChargeDamage`
+    * @see `ChargeFireModeDefinition`
+    * @see `ProjectileQuality`
+    * @see `ResolvedProjectile`
+    */
   case object SpikerChargeDamage extends Mod {
     def Calculate: DamageModifiers.Format = formula
 
@@ -437,6 +329,195 @@ object DamageModifiers {
         case _ =>
           damage
       }
+    }
+  }
+
+  /**
+    * If the damage is resolved through a `HitDamage` packet,
+    * calculate the damage as a function of its degrading value over distance traveled by its carrier projectile.
+    * @see `distanceDegradeFunction`
+    * @see `ProjectileQuality`
+    * @see `ResolvedProjectile`
+    */
+  case object FlakHit extends Mod {
+    def Calculate: DamageModifiers.Format = formula
+
+    private def formula(damage: Int, data: ResolvedProjectile): Int = {
+      if(data.resolution == ProjectileResolution.Hit) {
+        distanceDegradeFunction(damage, data)
+      } else {
+        damage
+      }
+    }
+  }
+
+  /**
+    * If the damage is resolved through a `SplashHitDamage` packet,
+    * calculate the damage as a function of its degrading value over distance
+    * between the hit position of the projectile and the position of the target.
+    * @see `radialDegradeFunction`
+    * @see `ProjectileQuality`
+    * @see `ResolvedProjectile`
+    */
+  case object FlakBurst extends Mod {
+    def Calculate: DamageModifiers.Format = formula
+
+    private def formula(damage: Int, data: ResolvedProjectile): Int = {
+      if(data.resolution == ProjectileResolution.Splash) {
+        radialDegradeFunction(damage, data)
+      } else {
+        damage
+      }
+    }
+  }
+
+  /* Functions */
+
+  /**
+    * The input value degrades (lessens)
+    * the further the distance between the point of origin (`shot_origin`)
+    * and the point of encounter (`hit_pos`) of its vector (projectile).
+    * If the value is not set to degrade over any distance within its maximum distance, the value goes unmodified.
+    * If the value is encountered beyond its maximum distance, the value is zero'd.
+    */
+  private def distanceDegradeFunction(damage: Int, data: ResolvedProjectile): Int = {
+    val projectile = data.projectile
+    val profile    = projectile.profile
+    val distance   = Vector3.Distance(data.hit_pos, projectile.shot_origin)
+    if (distance <= profile.DistanceMax) {
+      if (profile.DistanceNoDegrade == profile.DistanceMax || distance <= profile.DistanceNoDegrade) {
+        damage
+      } else {
+        damage - ((damage - profile.DegradeMultiplier * damage) * ((distance - profile.DistanceNoDegrade) / (profile.DistanceMax - profile.DistanceNoDegrade))).toInt
+      }
+    } else {
+      0
+    }
+  }
+
+  /**
+    * The input value degrades (lessens)
+    * the further the distance between the point of origin (target position)
+    * and the point of encounter (`hit_pos`) of its vector (projectile).
+    * If the value is encountered beyond its maximum radial distance, the value is zero'd.
+    */
+  private def radialDegradeFunction(damage: Int, data: ResolvedProjectile): Int = {
+    val profile   = data.projectile.profile
+    val distance  = Vector3.Distance(data.hit_pos, data.target.Position)
+    val radius    = profile.DamageRadius
+    val radiusMin = profile.DamageRadiusMin
+    if (distance <= radiusMin) {
+      damage
+    } else if (distance <= radius) {
+      //damage - (damage * profile.DamageAtEdge * (distance - radiusMin) / (radius - radiusMin)).toInt
+      val base = profile.DamageAtEdge
+      val radi = radius - radiusMin
+      (damage * ((1 - base) * ((radi - (distance - radiusMin)) / radi) + base)).toInt
+    } else {
+      0
+    }
+  }
+
+  /**
+    * For damage application that involves aggravation of a particular damage type,
+    * calculate that initial damage application for infantry targets
+    * and produce the modified damage value.
+    * Infantry wearing mechanized assault exo-suits (MAX) incorporate an additional modifier.
+    * @see `AggravatedDamage`
+    * @see `ExoSuitType`
+    * @see `InfantryAggravatedDirect`
+    * @see `InfantryAggravatedSplash`
+    * @see `PlayerSource`
+    * @see `ProjectileTarget.AggravatesTarget`
+    * @see `ResolvedProjectile`
+    * @param resolution the projectile resolution to match against
+    * @param damageType the damage type to find in as a component of aggravated information
+    * @param damage the base damage value
+    * @param data historical information related to the damage interaction
+    * @return the modified damage
+    */
+  private def baseAggravatedFormula(
+                                     resolution: ProjectileResolution.Value,
+                                     damageType : DamageType.Value
+                                   )
+                                   (
+                                     damage: Int,
+                                     data: ResolvedProjectile
+                                   ): Int = {
+    if (data.resolution == resolution &&
+        data.projectile.quality == ProjectileQuality.AggravatesTarget) {
+      (data.projectile.profile.Aggravated, data.target) match {
+        case (Some(aggravation), p: PlayerSource) =>
+          val aggravatedDamage = aggravation.info.find(_.damage_type == damageType) match {
+            case Some(infos) =>
+              damage * infos.degradation_percentage + damage
+            case _ =>
+              damage toFloat
+          }
+          if(p.ExoSuit == ExoSuitType.MAX) {
+            (aggravatedDamage * aggravation.max_factor) toInt
+          } else {
+            aggravatedDamage toInt
+          }
+        case _ =>
+          damage
+      }
+    } else {
+      damage
+    }
+  }
+
+  /**
+    * For damage application that involves aggravation of a particular damage type,
+    * calculate that damage application burn for each tick for infantry targets
+    * and produce the modified damage value.
+    * Infantry wearing mechanized assault exo-suits (MAX) incorporate an additional modifier.
+    * Vanilla infantry incorporate their resistance value into a slightly different calculation than usual.
+    * @see `AggravatedDamage`
+    * @see `ExoSuitType`
+    * @see `InfantryAggravatedDirectBurn`
+    * @see `InfantryAggravatedSplashBurn`
+    * @see `PlayerSource`
+    * @see `ResolvedProjectile`
+    * @param resolution the projectile resolution to match against
+    * @param damageType the damage type to find in as a component of aggravated information
+    * @param damage the base damage value
+    * @param data historical information related to the damage interaction
+    * @return the modified damage
+    */
+  private def baseAggravatedBurnFormula(
+                                         resolution: ProjectileResolution.Value,
+                                         damageType : DamageType.Value
+                                       )
+                                       (
+                                         damage: Int,
+                                         data: ResolvedProjectile
+                                       ): Int = {
+    if (data.resolution == resolution) {
+      (data.projectile.profile.Aggravated, data.target) match {
+        case (Some(aggravation), p: PlayerSource) =>
+          val degradation = aggravation.info.find(_.damage_type == damageType) match {
+            case Some(info) =>
+              info.degradation_percentage
+            case _ =>
+              1f
+          }
+          if (p.exosuit == ExoSuitType.MAX) {
+            (damage * degradation * aggravation.max_factor) toInt
+          } else {
+            val resist = data.damage_model.ResistUsing(data)(data)
+            //add resist to offset resist subtraction later
+            if (damage > resist) {
+              ((damage - resist) * degradation).toInt + resist
+            } else {
+              (damage * degradation).toInt + resist
+            }
+          }
+        case _ =>
+          0
+      }
+    } else {
+      damage
     }
   }
 }
