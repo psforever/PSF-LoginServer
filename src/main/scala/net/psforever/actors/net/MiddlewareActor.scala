e424c1a56bdfc564333dde32cf1ee1198ec10020
Jason_DiDonato@yahoo.com <Jason_DiDonato@yahoo.com>
2020-12-30 17:11:06 -0500

    adjustment to bundle dispatch timing; adjustment to inbound sequence reorder queue

--- src/main/scala/net/psforever/actors/net/MiddlewareActor.scala
+++ src/main/scala/net/psforever/actors/net/MiddlewareActor.scala
@@ -9,7 +9,7 @@ import akka.io.Udp
 import net.psforever.packet.{CryptoPacketOpcode, PacketCoding, PlanetSideControlPacket, PlanetSideCryptoPacket, PlanetSideGamePacket, PlanetSidePacket}
 import net.psforever.packet.control.{ClientStart, ConnectionClose, ControlSync, ControlSyncResp, HandleGamePacket, MultiPacket, MultiPacketEx, RelatedA, RelatedB, ServerStart, SlottedMetaPacket, TeardownConnection}
 import net.psforever.packet.crypto.{ClientChallengeXchg, ClientFinished, ServerChallengeXchg, ServerFinished}
-import net.psforever.packet.game.{ChangeFireModeMessage, CharacterInfoMessage, KeepAliveMessage, PingMsg}
+import net.psforever.packet.game.{CharacterInfoMessage, KeepAliveMessage, PingMsg}
 import scodec.Attempt.{Failure, Successful}
 import scodec.bits.{BitVector, ByteVector, HexStringSyntax}
 import scodec.interop.akka.EnrichedByteVector
@@ -21,7 +21,6 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider
 import scodec.Attempt
 
 import scala.collection.mutable
-import scala.collection.mutable.ListBuffer
 import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
 import scala.concurrent.duration._
 
@@ -57,6 +56,9 @@ object MiddlewareActor {
 
   /** Close connection */
   final case class Close() extends Command
+
+  /** Log inbound packets that are yet to be in proper order by sequence number */
+  private case class InReorderEntry(packet: PlanetSidePacket, sequence: Int, time: Long)
 
   /**
     * All packets are bundled by themselves.
@@ -112,13 +114,18 @@ class MiddlewareActor(
     context.spawnAnonymous(next(context.self, sender, connectionId), ActorTags(s"id=$connectionId"))
 
   /** Queue of incoming packets (plus sequence numbers and timestamps) that arrived in the wrong order */
-  val inReorderQueue: ListBuffer[(PlanetSidePacket, Int, Long)] = ListBuffer()
+  private val inReorderQueue: mutable.Queue[InReorderEntry] = mutable.Queue()
 
   /** Latest incoming sequence number */
   var inSequence = 0
 
   /** Latest incoming subslot number */
   var inSubslot = 0
+
+  /** the last confirmed packet `inSubslot` number
+    * @see `RelatedB`
+    */
+  var inSubslotConfirmation: Int = 0
 
   /** List of missing subslot numbers and attempts counter */
   var inSubslotsMissing: mutable.Map[Int, Int] = mutable.Map()
@@ -186,12 +193,16 @@ class MiddlewareActor(
     packet
   }
 
+  /** Delay between runs of the queue processor (ms) */
+  val queueProcessorHz = 10.milliseconds
+
   /** Timer that handles the bundling and throttling of outgoing packets and the reordering of incoming packets */
   val queueProcessor: Cancellable = {
-    context.system.scheduler.scheduleWithFixedDelay(10.milliseconds, 10.milliseconds)(() => {
+    context.system.scheduler.scheduleWithFixedDelay(queueProcessorHz, queueProcessorHz)(() => {
       try {
-
-        if (outQueue.nonEmpty && outQueueBundled.isEmpty) {
+        if (outQueueBundled.nonEmpty) {
+          sendFirstBundle()
+        } else if (outQueue.nonEmpty) {
           val bundle = {
             var length = 0L
             val (_, bundle) = outQueue
@@ -223,61 +234,86 @@ class MiddlewareActor(
           }
 
           if (bundle.length == 1) {
-            outQueueBundled.enqueueAll(splitPacket(bundle.head))
+            splitPacket(bundle.head) match {
+              case Seq() => ;
+                log.warn("")
+                //TODO oversized packet recovery?
+              case data =>
+                outQueueBundled.enqueueAll(data)
+                sendFirstBundle()
+            }
           } else {
             PacketCoding.encodePacket(MultiPacketEx(bundle.toVector.map(_.bytes))) match {
-              case Successful(data) => outQueueBundled.enqueue(smp(0, data.bytes))
-              case Failure(cause)   => log.error(cause.message)
-            }
-          }
-        }
-
-        outQueueBundled.dequeueFirst(_ => true) match {
-          case Some(packet) => send(packet, Some(nextSequence), crypto)
-          case None         => ;
-        }
-
-        if (inReorderQueue.nonEmpty) {
-          var currentSequence = inSequence
-          val currentTime     = System.currentTimeMillis()
-          inReorderQueue
-            .sortBy(_._2)
-            .dropWhile {
-              case (_, sequence, time) =>
-                // Forward packet if next in sequence order or older than 20ms
-                if (sequence == currentSequence + 1 || currentTime - time > 20) {
-                  currentSequence += 1
-                  true
-                } else {
-                  false
+              case Successful(data) =>
+                outQueueBundled.enqueue(smp(slot = 0, data.bytes))
+                sendFirstBundle()
+              case Failure(cause)   =>
+                log.error(cause.message)
+                //to avoid packets being lost, unwrap bundle and attempt to queue the packets individually
+                bundle.foreach { packet =>
+                  outQueueBundled.enqueue(smp(slot = 0, packet.bytes))
                 }
+                sendFirstBundle()
             }
-            .foreach {
-              case (packet, sequence, _) =>
-                if (sequence > inSequence) {
-                  inSequence = sequence
-                }
-                in(packet)
-            }
-        }
-
-        if (inSubslotsMissing.nonEmpty) {
-          inSubslotsMissing.foreach {
-            case (subslot, attempts) =>
-              if (attempts <= 50) {
-                // Slight hack to send RelatedA less frequently, might want to put this on a separate timer
-                if (attempts % 10 == 0) send(RelatedA(0, subslot))
-                inSubslotsMissing(subslot) += 1
-              } else {
-                log.warn(s"Requesting subslot $subslot from client failed")
-                inSubslotsMissing.remove(subslot)
-              }
           }
         }
       } catch {
-        case e: Throwable => log.error(e)("Queue processing error")
+        case e: Throwable =>
+          log.error(s"outbound queue processing error - ${Option(e.getMessage).getOrElse(e.getClass.getSimpleName)}")
+      }
+
+      if (inReorderQueue.nonEmpty) {
+        handleInReorderQueue()
+      }
+
+      inSubslotsMissing.foreach {
+        case (subslot, attempts) =>
+          if (attempts <= 50) {
+            // Slight hack to send RelatedA less frequently, might want to put this on a separate timer
+            if (attempts % 10 == 0) send(RelatedA(0, subslot))
+            inSubslotsMissing(subslot) += 1
+          } else {
+            log.warn(s"requesting subslot $subslot from client failed")
+            inSubslotsMissing.remove(subslot)
+          }
       }
     })
+  }
+
+  /**
+    * Take the first fully-prepared packet from the queue of prepared packets and send it to the client.
+    * Assign the dispatched packet the latest sequence number.
+    * Do not call unless confident the the queue has at least one element.
+    * @throws NoSuchElementException if there is no packet to dequeue
+    * @see `SlottedMetaPacket`
+    */
+  def sendFirstBundle(): Unit = {
+    send(outQueueBundled.dequeue(), Some(nextSequence), crypto)
+  }
+
+  /**
+    * Examine inbound packets that need to be reordered by sequence number and
+    * pass all packets that are now in the correct order and
+    * pass packets that have been kept waiting for too long in the queue.
+    * Set the recorded inbound sequence number to belong to the greatest packet removed from the queue.
+    */
+  def handleInReorderQueue(): Unit = {
+    var currentSequence  = inSequence
+    val currentTime      = System.currentTimeMillis()
+    val take = inReorderQueue
+      .takeWhile { entry =>
+        // Forward packet if next in sequence order or older than 50ms (five update attempts)
+        if (entry.sequence == currentSequence + 1 || currentTime - entry.time > 50) {
+          currentSequence = entry.sequence //already ordered by sequence, stays in order during traversal
+          true
+        } else {
+          false
+        }
+      }
+      .map(_.packet)
+    inReorderQueue.takeRightInPlace(inReorderQueue.length - take.length)
+    inSequence = currentSequence
+    take.foreach { in }
   }
 
 //CryptoSessionActor
@@ -309,9 +345,6 @@ class MiddlewareActor(
                   case ping: PingMsg =>
                     // reflect the packet back to the sender
                     send(ping)
-                    Behaviors.same
-                  case _: ChangeFireModeMessage =>
-                    // ignore
                     Behaviors.same
                   case _ =>
                     log.error(s"Unexpected non-crypto packet type $packet in start")
@@ -447,11 +480,20 @@ class MiddlewareActor(
               if (sequence == inSequence + 1) {
                 inSequence = sequence
                 in(packet)
+              } else if(sequence <= inSequence) { //expedite this packet
+                in(packet)
               } else {
-                inReorderQueue.addOne((packet, sequence, System.currentTimeMillis()))
+                var insertAtIndex = 0
+                val length = inReorderQueue.length
+                while (insertAtIndex < length && sequence >= inReorderQueue(insertAtIndex).sequence) {
+                  insertAtIndex += 1
+                }
+                inReorderQueue.insert(insertAtIndex, InReorderEntry(packet, sequence, System.currentTimeMillis()))
               }
-            case Successful((packet, None)) => in(packet)
-            case Failure(e)                 => log.error(s"Could not decode packet: $e")
+            case Successful((packet, None)) =>
+              in(packet)
+            case Failure(e)                 =>
+              log.error(s"could not decode packet: $e")
           }
           Behaviors.same
 
@@ -467,7 +509,7 @@ class MiddlewareActor(
         case Close() =>
           outQueue
             .dequeueAll(_ => true)
-            .foreach(p => send(smp(0, p._2.bytes), Some(nextSequence), crypto))
+            .foreach(p => send(smp(slot = 0, p._2.bytes), Some(nextSequence), crypto))
           connectionClose()
       }
       .receiveSignal(onSignal)
@@ -512,7 +554,7 @@ class MiddlewareActor(
             Behaviors.same
 
           case RelatedA(slot, subslot) =>
-            log.trace(s"Client indicated a smp of slot $slot is missing prior to subslot $subslot")
+            log.warn(s"Client indicated a smp of slot $slot is missing prior to subslot $subslot")
             val requestedSubslot = subslot - 1
             preparedSlottedMetaPackets.find(_.subslot == requestedSubslot) match {
               case Some(_packet) =>
@@ -624,14 +666,13 @@ class MiddlewareActor(
     if (packet.length > (MTU - 4) * 8) {
       PacketCoding.encodePacket(HandleGamePacket(packet.bytes)) match {
         case Successful(data) =>
-          data.grouped((MTU - 8) * 8).map(vec => smp(4, vec.bytes)).toSeq
+          data.grouped((MTU - 8) * 8).map(vec => smp(slot = 4, vec.bytes)).toSeq
         case Failure(cause) =>
           log.error(cause.message)
           Seq()
       }
     } else {
-      Seq(smp(0, packet.bytes))
+      Seq(smp(slot = 0, packet.bytes))
     }
   }
-
 }
