d0e6c44e587b0cf71454f47bd46faadc19288f79
Mazo <mazo2@hotmail.com>
2021-01-26 22:57:44 +0000

    CaptureFlag + CaptureFlagSocket objects/converters/definitions

---
+++ src/main/scala/net/psforever/objects/serverobject/llu/CaptureFlagDefinition.scala
@@ -0,0 +1,11 @@
+package net.psforever.objects.serverobject.llu
+
+import net.psforever.objects.serverobject.structures.AmenityDefinition
+
+/**
+  * The definition for any 'CaptureFlag'
+  * Object Id 157
+  */
+class CaptureFlagDefinition extends AmenityDefinition(157) {
+  Name = "capture_flag"
+}
---
+++ src/main/scala/net/psforever/objects/serverobject/llu/CaptureFlagSocket.scala
@@ -0,0 +1,32 @@
+package net.psforever.objects.serverobject.llu
+
+import akka.actor.ActorContext
+import net.psforever.objects.GlobalDefinitions
+import net.psforever.objects.serverobject.structures.Amenity
+import net.psforever.types.Vector3
+
+
+/**
+  * Represents the LLU sockets found within bases that require LLU hacks.
+  * It is used as a position reference for spawning the LLU in the correct location when the base is hacked
+  * @param tDef the `ObjectDefinition` that constructs this object and maintains some of its immutable fields
+  */
+class CaptureFlagSocket(tDef: CaptureFlagSocketDefinition) extends Amenity {
+  def Definition : CaptureFlagSocketDefinition = tDef
+}
+
+object CaptureFlagSocket {
+  def apply(tDef: CaptureFlagSocketDefinition) : CaptureFlagSocket = {
+    new CaptureFlagSocket(tDef)
+  }
+
+  def Constructor(pos: Vector3)(id: Int, context: ActorContext) : CaptureFlagSocket = {
+    Constructor(GlobalDefinitions.llm_socket, pos)(id, context)
+  }
+
+  def Constructor(tdef: CaptureFlagSocketDefinition, pos: Vector3)(id: Int, context: ActorContext) : CaptureFlagSocket = {
+    val obj = CaptureFlagSocket(tdef)
+    obj.Position = pos
+    obj
+  }
+}
