8d0b8648a22ccc4512d4ba87789d41b20f5e69c3
Fate-JH <Jason_DiDonato@yahoo.com>
2023-10-08 12:20:20 -0400

    removing the OF statement in a trigger

--- server/src/main/resources/db/migration/V008__Scoring.sql
+++ server/src/main/resources/db/migration/V008__Scoring.sql
@@ -399,7 +399,6 @@ $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER psf_account_newSession
 AFTER UPDATE
-OF avatar_logged_in
 ON account
 FOR EACH ROW
 WHEN (OLD.avatar_logged_in = 0 AND NEW.avatar_logged_in > 0)
