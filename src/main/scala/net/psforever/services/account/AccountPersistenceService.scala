689f67ec3972466ac2d71e7c72a6d541d6435640
ScrawnyRonnie <scrawnyronniebbac@yahoo.com>
2023-12-09 07:08:04 -0500

    drop the llu

--- src/main/scala/net/psforever/services/account/AccountPersistenceService.scala
+++ src/main/scala/net/psforever/services/account/AccountPersistenceService.scala
@@ -365,6 +365,11 @@ class PersistenceMonitor(
   def PerformLogout(): Unit = {
     (inZone.Players.find(p => p.name == name), inZone.LivePlayers.find(p => p.Name == name)) match {
       case (Some(avatar), Some(player)) if player.VehicleSeated.nonEmpty =>
+        //in case the player is holding the llu and disconnects
+        val zone         = player.Zone
+        val events       = zone.AvatarEvents
+        val nameChannel  = player.Name
+        events ! AvatarServiceMessage(nameChannel, AvatarAction.DropSpecialItem())
         //alive or dead in a vehicle
         //if the avatar is dead while in a vehicle, they haven't released yet
         AvatarActor.saveAvatarData(avatar)
@@ -381,6 +386,11 @@ class PersistenceMonitor(
         PlayerAvatarLogout(avatar, player)
 
       case (Some(avatar), Some(player)) =>
+        //in case the player is holding the llu and disconnects
+        val zone         = player.Zone
+        val events       = zone.AvatarEvents
+        val nameChannel  = player.Name
+        events ! AvatarServiceMessage(nameChannel, AvatarAction.DropSpecialItem())
         //alive or dead, as standard Infantry
         AvatarActor.saveAvatarData(avatar)
         AvatarActor.finalSavePlayerData(player)
