f2001dbc5f72cbc42d2c556a24494cadf91162c0
Resaec <resaecscript@googlemail.com>
2025-08-17 18:58:36 +0200

    random values :(

--- src/main/scala/net/psforever/packet/game/OutfitMemberEvent.scala
+++ src/main/scala/net/psforever/packet/game/OutfitMemberEvent.scala
@@ -14,11 +14,15 @@ final case class OutfitMemberEvent(
   unk5: Int,
   unk6: Int,
   member_name: String,
-  unk7: Int,
   unk8: Int,
   unk9: Int,
   unk10: Int,
   unk11: Int,
+  unk12: Int,
+  unk13: Int,
+  unk14: Int,
+  unk15: Int,
+  unk16: Int,
 ) extends PlanetSideGamePacket {
   type Packet = OutfitMemberEvent
   def opcode = GamePacketOpcode.OutfitMemberEvent
@@ -34,19 +38,23 @@ object OutfitMemberEvent extends Marshallable[OutfitMemberEvent] {
       ("unk5" | uint8L) ::
       ("unk6" | uint8L) ::
       ("member_name" | PacketHelpers.encodedWideStringAligned(6)) ::
-      ("unk7" | uint16L) ::
-      ("unk8" | uint16L) ::
-      ("unk9" | uint16L) ::
-      ("unk10" | uint16L) ::
-      ("unk11" | uint8L)
+      ("unk8" | uint8L) ::
+      ("unk9" | uint8L) ::
+      ("unk10" | uint8L) ::
+      ("unk11" | uint8L) ::
+      ("unk12" | uint8L) ::
+      ("unk13" | uint8L) ::
+      ("unk14" | uint8L) ::
+      ("unk15" | uint8L) ::
+      ("unk16" | uint8L)
     ).xmap[OutfitMemberEvent](
     {
-      case unk00 :: outfit_id :: u3 :: u4 :: u5 :: u6 :: member_name :: u7 :: u8 :: u9 :: u10 :: u11 :: HNil =>
-        OutfitMemberEvent(unk00, outfit_id, u3, u4, u5, u6, member_name, u7, u8, u9, u10, u11)
+      case unk00 :: outfit_id :: u3 :: u4 :: u5 :: u6 :: member_name :: u8 :: u9 :: u10 :: u11 :: u12 :: u13 :: u14 :: u15 :: u16 :: HNil =>
+        OutfitMemberEvent(unk00, outfit_id, u3, u4, u5, u6, member_name, u8, u9, u10, u11, u12, u13, u14, u15, u16)
     },
     {
-      case OutfitMemberEvent(unk00, outfit_id, u3, u4, u5, u6, member_name, u7, u8, u9, u10, u11) =>
-        unk00 :: outfit_id :: u3 :: u4 :: u5 :: u6 :: member_name :: u7 :: u8 :: u9 :: u10 :: u11 :: HNil
+      case OutfitMemberEvent(unk00, outfit_id, u3, u4, u5, u6, member_name, u8, u9, u10, u11, u12, u13, u14, u15, u16) =>
+        unk00 :: outfit_id :: u3 :: u4 :: u5 :: u6 :: member_name :: u8 :: u9 :: u10 :: u11 :: u12 :: u13 :: u14 :: u15 :: u16 :: HNil
     }
   )
 }
